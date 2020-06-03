ca9f0370864e2bd17b56b8e212b56d3bddfb9172
jamie <jamie@cntt.co.kr>
2020-06-03 16:52:14 +0900

    LinkedList 개체 생성

--- src/list/linkedlist/implementation/LinkedList.java
+++ src/list/linkedlist/implementation/LinkedList.java
@@ -1,11 +1,22 @@
 package list.linkedlist.implementation;
 
 public class LinkedList {
-    public LinkedList() {
-        System.out.println("생성자");
-    }
+    private Node head;
+    private Node tail;
+    private int size = 0;
 
-    public void test(){
-        System.out.println("뭐지");
+    private class Node {
+        private Object data;
+        private Node next;
+
+        public Node(Object data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(this.data);
+        }
     }
 }
